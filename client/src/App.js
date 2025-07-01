9614d620bc27e34fec49a5bc999bcc0508f81f72
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2025-06-30 17:07:24 -0700

    starting new

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 className="text-4xl font-bold text-green-500">Weed Letterboxd</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
